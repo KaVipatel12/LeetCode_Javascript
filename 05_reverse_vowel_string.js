911e3ab853f089e5e72e184f359355b9843d009b
kush <email@gmail.com>
2025-08-06 14:47:19 +0530

    05 : Reverse the vowel of the string

---
+++ 05_reverse_vowel_string.js
@@ -0,0 +1,25 @@
+var reverseVowels = function(s) {
+ 
+    const vowels = new Set([ "a" , "e" , "i" , "o" , "u", "A" , "E" , "I" , "O" , "U"]); 
+    let vowelsExist = []
+    let counter = 0; 
+    for (let i = 0; i < s.length; i++) {
+        if(vowels.has(s[i])){
+            vowelsExist.push(s[i])
+        }        
+    }
+    vowelsExist.reverse(); 
+    let newStr = ""
+    for (let i = 0; i < s.length; i++) {
+        if(vowels.has(s[i])){
+            newStr += vowelsExist[counter]
+            counter++
+        }else{
+            newStr += s[i]
+        }
+    }
+
+    return newStr; 
+};
+
+console.log(reverseVowels("Parle Gi"))
